c7db93dd757331b0915ba8d290efdb383b4f06f9
Rashmib39 <belavatagirashmi@gmail.com>
2024-09-01 12:13:00 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+big3()
+{
+    int num1, num2, temp;
+    
+    num1=20,num2=30;
+    if(num1 == num2)
+        printf("Both are Equal\n");
+    else{
+        temp = num1 > num2? num1 : num2;
+        printf("%d is largest",temp);
+    }
+  return 0;
+}
